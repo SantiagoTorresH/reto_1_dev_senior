bb9ed97593a380a3440c3ebb98fdf85738f2d705
SantiagoT <122948175+SantiagoTorresH@users.noreply.github.com>
2024-11-17 21:05:48 -0500

    ramaSan

---
+++ main.py
@@ -0,0 +1,79 @@
+import datetime
+
+'''Atributos de un experimento:nombre, fecha, tipo, resultados numericos'''
+
+listaDeExperimentos = [
+    ["Esperimento 1", "16/11/2024", "Quimica",[5,2,3,56,7]],
+
+    
+]
+
+
+listaDeExperimentos2 = []
+
+def agregar_experimetos():
+    """Permite agregar un nuevo experimento con sus atributos. dificultad: 1"""
+    """Permite agregar un nuevo experimento con sus atributos. dificultad: 1""" 
+    """Permite agregar un nuevo experimento con sus atributos. dificultad: 1""" 
+    
+    pass
+
+
+def eliminar_experimentos():
+    """Perminte eliminar un experimento. dificultad: 1, requiere el uso de la funcion agregar_experimento"""
+    
+    pass
+
+
+def visualizar_experimentos():
+    """Permite visualizar todos los experimentos. dificultad: 1. requiere el uso de la funcion agregar_experimento"""
+    
+    pass
+
+def calcular_Estadisticas():
+    """Calcular estadisticas basicas(promedios, máximos y mínimos) de un experimento. dificultad: 2. requiere el uso de funcion agregar_experimento"""
+    pass
+
+def comparar_experimentos():
+    '''compara dos o mas experimentos para determinar los mejores o peores resultados'''
+
+def generar_informe():
+    """Generar un informe resumen de los expremintos y sus estadisticas. dificultad: 3. requiere el uso de funciones visualizar_experimentos y calcular_estadisticas"""
+    
+    pass
+
+def mostrar_menu():
+    print("===Menu Principal====")
+    
+    print("===Gestion de experimentos====")
+    print("1. Agregar experimento")
+    print("2. Visualizar experimentos")
+    print("3. Eliminar experimentos")
+    
+    print("===Analisis de datos====")
+    print("4. calcular estadisticas ")
+    print("5. comparar experimentos ")
+    
+    print("===Informes====")
+    print("6. generar Informe")
+    print("====Salir====")
+    print("7. Salir")
+    
+    
+    
+    pass
+
+def main():
+    """Controla el flujo general del sistema. dificultad: 1"""
+    mostrar_menu()   
+    pass
+
+main()
+
+
+# if __name__ == "__main__":  # es lo mismo al main pero mas avanzado, 
+#     main()
+
+
+
+    
