dc4fba589d7ac684f66b9beb4688bb4df4bdb874
SantiagoT <122948175+SantiagoTorresH@users.noreply.github.com>
2024-11-17 21:04:53 -0500

    ramas
    ramas

---
+++ main.py
@@ -0,0 +1,71 @@
+import datetime
+
+'''Atributos de un experimento:nombre, fecha, tipo, resultados numericos'''
+
+listaDeExperimentos = [
+    ["Esperimento 1", "16/11/2024", "Quimica",[5,2,3,56,7]],
+    
+]
+
+listaDeExperimentos2 = []
+
+def agregar_experimetos():
+    
+    pass
+
+
+def eliminar_experimentos():
+    
+    pass
+
+
+def visualizar_experimentos():
+    
+    pass
+
+def calcular_Estadisticas():
+    
+    pass
+
+def comparar_experimentos():
+    '''compara dos o mas experimentos para determinar los mejores o peores resultados'''
+
+def generar_informe():
+    
+    pass
+
+def mostrar_menu():
+    print("===Menu Principal====")
+    
+    print("===Gestion de experimentos====")
+    print("1. Agregar experimento")
+    print("2. Visualizar experimentos")
+    print("3. Eliminar experimentos")
+    
+    print("===Analisis de datos====")
+    print("4. calcular estadisticas ")
+    print("5. comparar experimentos ")
+    
+    print("===Informes====")
+    print("6. generar Informe")
+    print("====Salir====")
+    print("7. Salir")
+    
+    
+    
+    pass
+
+def main():
+    """Controla el flujo general del sistema. dificultad: 1"""
+    mostrar_menu()   
+    pass
+
+main()
+
+
+# if __name__ == "__main__":  # es lo mismo al main pero mas avanzado, 
+#     main()
+
+
+
+    
