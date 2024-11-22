9f677eaaada9e53f5cf95daa9ac103cb5233e76a
lin-acourtt <lindseyacourtt@gmail.com>
2024-11-21 20:37:12 -0500

    V2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 import statistics
 
 # Lindsey's branch.
@@ -20,11 +20,67 @@ def addExperiment(listOfExperiments):
     print("\n---------------------------------------------------")
     print("You will be entering the data for a new experiment.")
     print("---------------------------------------------------")
+    # String: It doesn't need verification.
     name = input("Experiment's name: ")
-    date = input("Date (dd/mm/yyyy): ") # Add date format verification
-    category = input("Category: ") # Change it to restrict user's input
-    results = list(input("List of results: ")) # Change to a for to avoid mistakes from user
 
+    # Datetime object: it needs verification with strptime
+    while True:
+        try:
+            date_input = input("Date (dd/mm/yyyy): ")
+            date = datetime.strptime(date_input, "%d/%m/%Y")
+            break
+        except ValueError:
+            print("Invalid format, please try again.")
+    
+    # String: User will not type it, it will be selected.
+    print("Select a number according to the category:")
+    print("1. Biology.")
+    print("2. Physics")
+    print("3. Chemistry")
+    # Verification required, to make sure the input is a valid integer
+    while True:
+        try:
+            categoryOpt = int(input("Type a number: ")) # Change it to restrict user's input
+            if categoryOpt == 1:
+                category = "Biology"
+                break
+            elif categoryOpt == 2:
+                category = "Physics"
+                break
+            elif categoryOpt == 3:
+                category = "Chemistry"
+                break
+            elif categoryOpt <=0:
+                print("Invalid input, please try again.")
+            else:
+                print("Input is a non-existent category, please try again")
+                continue
+        except:
+            print("Invalid input, please try again.")
+
+    # Float: verification required to make sure the input is a number
+    results = []
+    while True:
+        try:
+            numberOfResults = int(input("\nYou will be entering the results now. \nPlease enter the number of results (max 10): "))
+            if 0 <= numberOfResults <=10:
+                for i in range(numberOfResults):
+                    while True:
+                        try:
+                            results.append(float(input(f"Result #{i+1}: ")))
+                            break
+                        except:
+                            print("Error, the value is not a number, try again.")
+                break
+            else:
+                print("\nInvalid, input should be a positive integer.")
+                continue
+        except:
+            print("\nInvalid, input should be a positive integer.")
+
+
+    
+    # Object, the experiment is instanced. 
     thisExperiment = Experiment(name,date,category,results)
     listOfExperiments.append(thisExperiment)
     print("\nExperiment successfully added.")
@@ -32,6 +88,12 @@ def addExperiment(listOfExperiments):
 
 # This function will take the list of experiments called "listOfExperiments" as input
 def printExperiments(listOfExperiments):
+    # This function will display a message if there are no experiments to display.
+    if not listOfExperiments:
+        print("No experiments to display.")
+        input("\nPress 'Enter' to continue...")
+        return
+
     print("----------------------------------------------------")
     print("This is the list of experiments you have documented.")
     
@@ -44,51 +106,121 @@ def printExperiments(listOfExperiments):
         print(f"Results: {thisExperiment.results}")
     input("\nPress Enter to continue...")
 
-def removeExperiment(listOfExperiments):
-    print("-----------------------------------")
-    print("You will be removing an experiment.")
-    print("-----------------------------------")
-    # Add verfications
-    experimentToRemove = int(input("Input the number of the experiment you want to remove, \nif you are not sure about the number, press 'v' to view the list of names: "))
-    del listOfExperiments[experimentToRemove-1]
-    print(f"\nExperiment #{experimentToRemove} removed successfully.")
-    input("\nPress Enter to continue...")
-
-# This function will take the list of experiments called "listOfExperiments" as input
-def selectExperiment(listOfExperiments):
+# This function will display the list of experiments (only by name)
+def displayExperimentsName(listOfExperiments):
     print("--------------------")
     print("List of experiments:")
     print("--------------------")
-    for i, thisExperiment in enumerate(listOfExperiments, start=1):
-        print("----------------------------------------------------")   
+    for i, thisExperiment in enumerate(listOfExperiments, start=1): 
         print(f"{i}. {thisExperiment.name}")
     print("--------------------")
 
+def removeExperiment(listOfExperiments):
+    # This function will display a message if there are no experiments to remove.
+    if not listOfExperiments:
+        print("No experiments registered yet.")
+        input("\nPress 'Enter' to continue...")
+        return
+    
+    print("-----------------------------------")
+    print("You will be removing an experiment.")
+    print("-----------------------------------")
+    # Make sure the input is an integer
+    while True:
+        try:
+            experimentToRemove = int(input("\nInput the number of the experiment you want to remove, \nif you are not sure about the number, press '0' to view the list of names: "))
+            # The number must be an integer between 1 and the number of experiments.
+            if 1 <= experimentToRemove <= len(listOfExperiments):
+                del listOfExperiments[experimentToRemove-1]
+                print(f"\nExperiment #{experimentToRemove} removed successfully.")
+                input("\nPress Enter to continue...")
+                break
+            elif experimentToRemove == 0:
+                displayExperimentsName(listOfExperiments)
+                continue
+            else:
+                print("\nNot a valid input. Please try again.")
+                continue
+        except:
+            print("\nError, the input is not an integer.")
+
+
 def calculateStatistics(listOfExperiments):
+    # This function will display a message if there are no experiments to remove.
+    if not listOfExperiments:
+        print("No experiments registered yet.")
+        input("\nPress 'Enter' to continue...")
+        return
+
     print("--------------------------------------------")
     print("Calculation of statistics for an experiment.")
     print("--------------------------------------------")    
-    expNumber = int(input("Select the number of the experiment. \nIf you are not sure about the number of the experiment, \npress 'v' to view the list: ")) # Add verification
-    print("--------------------------------------------")
-    print(f"The results for this experiment are: {listOfExperiments[expNumber-1].results}")
-    print(f"The average is: {statistics.mean(listOfExperiments[expNumber-1].results)}")
-    print(f"The minimun value is: {min(listOfExperiments[expNumber-1].results)}")
-    print(f"The maximum value is: {max(listOfExperiments[expNumber-1].results)}")
-    input("\nPress Enter to continue...")
+    
+    while True:
+        try:
+            expNumber = int(input("\nSelect the number of the experiment. \nIf you are not sure about the number of the experiment, \npress 0 to view the list: ")) 
+            # The number must be an integer between 1 and the number of experiments.
+            if 1 <= expNumber <= len(listOfExperiments):
+                print("--------------------------------------------")
+                print(f"The results for this experiment are: {listOfExperiments[expNumber-1].results}")
+                print(f"The average is: {statistics.mean(listOfExperiments[expNumber-1].results)}")
+                print(f"The minimun value is: {min(listOfExperiments[expNumber-1].results)}")
+                print(f"The maximum value is: {max(listOfExperiments[expNumber-1].results)}")
+                input("\nPress Enter to continue...")
+                break
+            elif expNumber == 0:
+                displayExperimentsName(listOfExperiments)
+                continue
+            else:
+                print("\nNot a valid input. Please try again.")
+                continue
+        except:
+            print("\nError, the input is not an integer.")
+
 
 def compareExperiments(listOfExperiments):
+    # This function will display a message if there are no experiments or if there is only one experiment.
+    if not listOfExperiments:
+        print("No experiments registered yet.")
+        input("\nPress 'Enter' to continue...")
+        return
+
+    elif len(listOfExperiments) == 1:
+        print("\nThere is only one experiment, nothing to compare.")
+        input("\nPress 'Enter' to continue...")
+        return
+    
     print("---------------------------------------")
     print("Comparision of two or more experiments.")
     print("---------------------------------------")
     listToCompare = []
+
+    # This loop will be used so that the user can input as many experiments as they need to compare.
     while True:
-        # Add verification (the number should not exceed the number of experiments)
-        # the input must be integer (try except)
-        experiment = int(input("Type the number of the experiment to compare, \nif you want to stop selecting experiments, type 0: "))
-        if experiment != 0:
-            listToCompare.append(experiment)
-        else:
-            break
+        try:
+            experiment = input("\nType the number of the experiment to compare, \nif you need to view the list of experiments, type 0, \nif you want to stop selecting experiments, type 'stop': ")
+            # First we need to validate if the number is lower that the number of registered experiments. 
+            try:
+                if 1 <= int(experiment) <= len(listOfExperiments):
+                    # Add this experiment to the list of experiments to compare.
+                    listToCompare.append(experiment)
+                    continue
+                elif int(experiment) == 0:
+                    displayExperimentsName(listOfExperiments)
+                    continue
+            except:
+                # Now, we will verified if the user selected at lest 2 experiments, if they didn't,
+                # they will be asked for more input
+                if len(set(listToCompare))<=1:
+                    if experiment.lower() == "stop":
+                        print("\nYou can't stop. Please enter another experiment. \nthere is only one experiment in the list.")
+                        continue
+                if experiment.lower() == "stop":
+                    break            
+             
+        except:
+            print("\nInvalid input, it must be an integer or 0.")
+
     listOfMeans = []
     listOfMins = []
     listOfMaxs = []
@@ -115,6 +247,7 @@ def compareExperiments(listOfExperiments):
     input("\nPress Enter to continue...")
 
 def generateReport(listOfExperiments):
+    # This function will display a message if there are no experiments.
     if not listOfExperiments:
         print("No experiments registered yet.")
         return
